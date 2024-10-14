1ae17b77ca29715f5a45c31b7d60089c5e68edb0
eliyan-mu <eliyanmousazadeh@hilma.tech>
2024-10-14 18:00:09 +0300

    server

--- js/server.js
+++ js/server.js
@@ -1,3 +1,31 @@
-function server (){
-    const user
+function server(object){
+const url = object.url
+const method = object.method
+
+ if(method==="GET"){
+    if(url==="my-server/api/logIn"){
+        login(clik);
+        return { status: 200, message: "Login successful" };
+    }
+    if(url==="my-server/api/signUp"){
+        addNewuser(name,password)
+        return { status: 200, message: "User added successfully" };
+    }
+    if (url=== "my-server/api/contact"){
+        addcontact(name,phone);
+        return { status: 200, message: "Contacts retrieved successfully" };
+    }
+
+ }
+ else if (method==="POST"){
+    if(url==="my-server/api/users"){
+        gettAllUser()
+        return { status: 200, message: "Users retrieved successfully" };
+    }
+    if(url==="my-server/api/getContact"){
+        getAllContact()
+        return { status: 200, message: "Contacts retrieved successfully" };
+    }
+ }
 }
+
