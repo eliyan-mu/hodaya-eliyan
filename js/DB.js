b7853a345abd64b1b3522cfcb160f6c4b8f2a3af
hodaya <hodayaadika@hilma.tech>
2024-10-14 15:21:06 +0300

    db

--- js/DB.js
+++ js/DB.js
@@ -1,32 +1,46 @@
 
-    // add contact
-    // remove contact
+    // add contact עשיתי
+    // remove contact עשיתי
     // add user עשיתי
-    // get  all user
-    // get all cantact
-    // current user
-    // chek usere if they exicit>>yiu are already exict 
+    // get  all user עשיתי
+    // get all cantact עשיתי
+    // current user עשיתי
+    // chek users if they exicit>>you are already exict 
     // if name=name >> you have to chnage the name 
-    // 
 
+    // class user{
+    //     constructor(name,password){
+    //         this.name=name;
+    //         this.password=password;
+    //         this.contact=[];
+    //     }
+    // }
 
-    class user{
-        constructor(name,password){
-            this.name=name;
-            this.password=password;
-            this.contact=[];
-        }
+    // class contact{
+    //     constructor(name,phone){
+    //         this.name=name;
+    //         this.phone=phone;
+    //     }
+    // }
+    function addcontact(name,phone){
+        let obj={name,phone};
+        let arr=JSON.parse(localStorage.getItem("contact"));
+        obj.id=arrpeople.length+1;
+        arr.push(obj);
+        localStorage.setItem('contact',JSON.stringify(arr));
     }
-
-    class contact{
-        constructor(name,phone){
-            this.name=name;
-            this.phone=phone;
+    function removecontact(num){
+        let arrship=JSON.parse(localStorage.getItem("contact"));
+        for(let i in arr){
+            if(parseInt(arr[i].id)===num){
+                arr[i]={"detail":"not found"}
+            }
         }
+        localStorage.setItem('contact',JSON.stringify(arr));
     }
+//sign-up
 
-
-    // chek if user exicted if not yu have to add it 
+    // chek if user exicted if not you have to add it 
     function addNewuser(name,password){
         if(JSON.parse(window.localStorage.getItem("users"))){
             const users = JSON.parse(window.localStorage.getItem("users"));
@@ -44,9 +58,27 @@
             window.localStorage.setItem("users",JSON.stringify(users));
         }
     }
+//add id to new user in sign up
+function addid(user){
+    let arr=JSON.parse(localStorage.getItem("users"));
+    user.id=arr.length;
+}
 
 function getAllContact(){
-    
+    let arr=JSON.parse(localStorage.getItem("contact"));
+    for(let i=0;i<arr.length;i++){
+        console.log(arr[i])
+    }
+}
+
+function getAllUsers(){
+    let arr=JSON.parse(localStorage.getItem("users"));
+    for(let i=0;i<arr.length;i++){
+        console.log(arr[i])
+    }
+}
+function printcurrent(){
+    console.log(JSON.parse(localStorage.getItem("currentUser")));
 }
 
    
